0ab4706c49c4dcf66492768240b0175d86d08365
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-03-14 02:21:56 -0700

    Adding elements to an empty list. The user is allowed to enter the number of items they want, and append to the list.

---
+++ python-practice/add_to_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+my_list = []
+number = int(input("Enter the number of list items: "))
+for i in range(number):
+    value = float(input("Enter the item value: "))
+    my_list.append(value)
+print(my_list)
