c71a147fef6e0b57c5192c1123af3f2722f36c9e
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-03-14 02:40:30 -0700

    sort the list items in ascending order i.e from the least to the biggest.

---
+++ python-practice/sortList.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# The code before the first print add element to
+# an empty list through user interaction.
+my_list = []
+number = int(input("Enter the number of list items: "))
+for i in range(number):
+    value = float(input("Enter the list item value: "))
+    my_list.append(value)
+print(my_list)
+
+# The code is now sorted in ascending order i.e
+# from the smallest number to the biggest.
+
+swapped = True
+while swapped:
+    swapped = False
+    for j in range(len(my_list) - 1):
+        if my_list[j] > my_list[j + 1]:
+            swappped = True
+            my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+print(\nSorted)
+print(my_list)
