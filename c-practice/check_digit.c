55fe8d5161983a342d0db8bba11d3f24d868767e
Sikiru Yaya <114338692+Brightsky2394@users.noreply.github.com>
2023-05-29 04:59:06 -0700

    check whether a digit appears more than once in a given number.

--- c-practice/check_digit.c
+++ c-practice/check_digit.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
  * check whether a digit appears
  * more than once in a given number.
  * if yes, print yes and otherwise no.
@@ -25,29 +24,4 @@ int main(void){
 	else
 		printf("No");
 	return (0);
-
-=======
-check whether a digit appears
-more than one times in a number.
-if yes, print yes and otherwise
-no.
-**/
-
-int main(void){
-  int seen[10] = {0}, num;
-  printf("Enter a number to be check: ");
-  scanf("%d", &num);
-  int rem;
-  while (num > 0){
-    rem = num % 10;
-    if (seen[rem] == 1)
-      break;
-    seen[rem] = 1;
-    num /= 10;
-  }
-  if (num > 0)
-    printf("Yes");
-  else
-    printf("No");
->>>>>>> 7069452011c104c5fb1514da7503f529812627fd
 }
